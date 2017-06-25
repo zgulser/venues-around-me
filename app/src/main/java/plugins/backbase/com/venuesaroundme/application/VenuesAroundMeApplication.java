cc19215e0288af477a36f65ff7bd8f3c53c93e60
Zeki Gulser <zeki@bb-system-0115.local>
2017-06-25 12:18:54 +0200

    initialization of the project

---
+++ app/src/main/java/plugins/backbase/com/venuesaroundme/application/VenuesAroundMeApplication.java
@@ -0,0 +1,15 @@
+package plugins.backbase.com.venuesaroundme.application;
+
+import android.app.Application;
+
+/**
+ * Created by Zeki on 25/06/2017.
+ */
+
+public class VenuesAroundMeApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/plugins/backbase/com/venuesaroundme/model/entities/Venue.java
@@ -0,0 +1,8 @@
+package plugins.backbase.com.venuesaroundme.model.entities;
+
+/**
+ * Created by Backbase R&D B.V on 25/06/2017.
+ */
+
+public class Venue {
+}
